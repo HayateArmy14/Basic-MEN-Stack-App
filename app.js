25090a5b903b7ff7bccdf37787897cbb3b06bd43
HayateArmy14 <pegadeli12345@gmail.com>
2021-06-30 09:53:02 +0300

    Add model Files

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require("passport");
+const User = require("./models/user");
+const session = require("express-session")
+const mongoose = require("mongoose")
 
+//require Routes
 const indexRouter = require('./routes/index');
 const posts = require("./routes/posts")
 const reviews = require("./routes/reviews")
 
 const app = express();
+
+//Connect to the Database 
+mongoose.connect('mongodb://localhost:27017/surf-shop', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to Database")
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +35,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configure Passport and Sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Mount Routes 
 app.use('/', indexRouter);
 app.use("/posts", posts);
 app.use("/posts/:id/reviews", reviews)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { postRegister } = require("../controllers/index")
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -12,9 +13,7 @@ router.get('/register', (req, res, next) => {
 });
 
 /* POST /register. */
-router.post('/register', (req, res, next) => {
-  res.send('POST /register');
-});
+router.post('/register', postRegister);
 
 /* GET /login. */
 router.get('/login', (req, res, next) => {
@@ -37,23 +36,24 @@ router.put('/profile/:user_id', (req, res, next) => {
 });
 
 /* GET /forgot-password. */
-router.get('/forgot-pw', (req, res, next) => {
-  res.send('GET /forgot-pw');
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot');
 });
 
 /* PUT /forgot-password. */
-router.put('/forgot-pw', (req, res, next) => {
-  res.send('PUT /forgot-pw');
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot');
 });
 
 /* GET /reset-password. */
-router.get('/reset-pw/:token', (req, res, next) => {
-  res.send('GET /reset-pw/:token');
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset/:token');
 });
 
 /* PUT /reset-password. */
-router.put('/reset-pw/:token', (req, res, next) => {
-  res.send('PUT /reset-pw/:token');
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset/:token');
 });
 
 module.exports = router;
+   
